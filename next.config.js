b06064fad136c6287f9f3b7083c65f8ba452587d
Nate Smith <nathangsmith18@gmail.com>
2020-09-30 18:11:27 -0500

    remove slack token

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const date = new Date();
 module.exports = {
     env: {
         ENV: process.env.ENV,
-        GOOGLE_TRACKING_ID: 'UA-36587141-17',
-        SLACK: {
-            API_TOKEN: 'xoxb-1318809134980-1336547187808-hCCLjSLFdo5NsAQOWR1BGZl7',
-            WEBSITE_CHANNEL_ID: 'C0196JASPQS'
-        },
         THEME: {
             PRIMARY: '#272a3d'
         },
